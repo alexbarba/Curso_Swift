a2246df48668bf5efc71f1643b144d452eae4fb8
alexbarba <alexbarba.2278@gmail.com>
2016-06-27 00:27:53 -0500

    Juego de memoria
    Tarea Semana 2 del curso de Swift

---
+++ juego_memoria.swift
@@ -0,0 +1,18 @@
+let a = 1...100
+for i in a{
+    //print(i)
+    if i%5==0{
+        print("#\(i) Bingo!!!")
+    }
+    
+    if i%2==0{
+        print("#\(i) par!!!")
+    }
+    else{
+        print("#\(i) impar!!!")
+    }
+    
+    if 20<=i&&i<=30{
+        print("#\(i) Viva Swift!!!")
+    }
+}
